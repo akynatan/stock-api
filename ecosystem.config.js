3db89743a3dc6afff4ec75cb6e80fd8179ed0dca
Aky Natan <akynatan@outlook.com>
2026-04-21 11:19:14 -0300

    test githubaction

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'stock-api',
       script: './node_modules/.bin/ts-node-dev',
       args: '-r tsconfig-paths/register --transpile-only --no-notify src/shared/infra/http/server.ts',
-      cwd: '/home/deploy/stock-api',
+      cwd: '/root/app/stock-api',
       env: {
         NODE_ENV: 'production',
         PORT: 4444,
